ec417f70d274ac7cc2f16798d64dc11dd26c99b2
Ico Doornekamp <ico@pruts.nl>
2019-10-06 14:59:36 +0200

    Performance test updates

--- tests/performance.nim
+++ tests/performance.nim
@@ -1,51 +1,65 @@
 
 import npeg
 import os
+import strutils
 import json
 import times
 import packedjson
 import osproc
 
-template measureTime*(what: string, code: untyped): float =
-  let start = cpuTime()
-  code
-  let duration = cpuTime() - start
-  echo what & " took ", duration, "s"
-  duration
-
-
-let s = peg "JSON":
-  S              <- *{' ','\t','\r','\n'}
-  True           <- "true"
-  False          <- "false"
-  Null           <- "null"
-
-  UnicodeEscape  <- 'u' * {'0'..'9','A'..'F','a'..'f'}[4]
-  Escape         <- '\\' * ({ '{', '"', '|', '\\', 'b', 'f', 'n', 'r', 't' } | UnicodeEscape)
-  StringBody     <- ?Escape * *( +( {'\x20'..'\xff'} - {'"'} - {'\\'}) * *Escape) 
-  String         <- ?S * '"' * StringBody * '"' * ?S
-
-  Minus          <- '-'
-  IntPart        <- '0' | {'1'..'9'} * *{'0'..'9'}
-  FractPart      <- "." * +{'0'..'9'}
-  ExpPart        <- ( 'e' | 'E' ) * ?( '+' | '-' ) * +{'0'..'9'}
-  Number         <- ?Minus * IntPart * ?FractPart * ?ExpPart
-
-  DOC            <- JSON * !1
-  JSON           <- ?S * ( Number | Object | Array | String | True | False | Null ) * ?S
-  Object         <- '{' * ( String * ":" * JSON * *( "," * String * ":" * JSON ) | ?S ) * "}"
-  Array          <- "[" * ( JSON * *( "," * JSON ) | ?S ) * "]"
-
 let js = execProcess("bzip2 -d < tests/json-32M.bzip2").string
 
-let tNpeg = measureTime "npeg":
-  echo s.match(js)
+template measureTime*(what: string, expect: float, code: untyped) =
+  let start = cpuTime()
+  block:
+    code
+  let duration = cpuTime() - start
+  echo what & ": ", duration.formatFloat(ffDecimal, 3), "s ", (duration/expect).formatFloat(ffDecimal, 3)
 
-let tPackedJson = measureTime "packedjson":
-  discard packedjson.parseJson(js)
 
-let tJson = measureTime "json":
-  discard json.parseJson(js)
+measureTime "json", 0.165:
 
-doAssert tNpeg < tPAckedJson
-doAssert tPackedJson < tJson
+  let p = peg "JSON":
+    S              <- *{' ','\t','\r','\n'}
+    True           <- "true"
+    False          <- "false"
+    Null           <- "null"
+
+    UnicodeEscape  <- 'u' * {'0'..'9','A'..'F','a'..'f'}[4]
+    Escape         <- '\\' * ({ '{', '"', '|', '\\', 'b', 'f', 'n', 'r', 't' } | UnicodeEscape)
+    StringBody     <- ?Escape * *( +( {'\x20'..'\xff'} - {'"'} - {'\\'}) * *Escape) 
+    String         <- ?S * '"' * StringBody * '"' * ?S
+
+    Minus          <- '-'
+    IntPart        <- '0' | {'1'..'9'} * *{'0'..'9'}
+    FractPart      <- "." * +{'0'..'9'}
+    ExpPart        <- ( 'e' | 'E' ) * ?( '+' | '-' ) * +{'0'..'9'}
+    Number         <- ?Minus * IntPart * ?FractPart * ?ExpPart
+
+    DOC            <- JSON * !1
+    JSON           <- ?S * ( Number | Object | Array | String | True | False | Null ) * ?S
+    Object         <- '{' * ( String * ":" * JSON * *( "," * String * ":" * JSON ) | ?S ) * "}"
+    Array          <- "[" * ( JSON * *( "," * JSON ) | ?S ) * "]"
+
+  echo p.match(js)
+
+
+measureTime "words", 2.20:
+
+  var v = 0
+  let p = peg foo:
+    foo <- +word
+    word <- @>+Alpha:
+      inc v
+  discard p.match(js).ok
+
+
+measureTime "search", 0.34:
+
+  var v = 0
+  let p = peg search:
+    search <- @"CALIFORNIA":
+      inc v
+  for i in 1..10:
+    discard p.match(js).ok
+
