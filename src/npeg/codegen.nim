e8022d351069c7fb76671f333cd5916eae0e8b13
Ico Doornekamp <ico@pruts.nl>
2019-12-22 17:38:17 +0100

    More refactoring of codegen. This finally gets rid of some nasty constructs with templates and codegen, and solves possible name clashes in code blocks

--- src/npeg/codegen.nim
+++ src/npeg/codegen.nim
@@ -120,10 +120,10 @@ when npegProfile:
   
 # Generate out all the case handlers for the parser program
 
-proc genCasesCode*(program: Program, userDataType: NimNode, userDataId: NimNode): NimNode =
+proc genCasesCode*(program: Program, userDataType: NimNode, userDataId: NimNode, ms, s, si, simax, ip: NimNode): NimNode =
   
   result = quote do:
-    case ip
+    case `ip`
 
   for ipNow, i in program.patt.pairs:
 
@@ -141,69 +141,69 @@ proc genCasesCode*(program: Program, userDataType: NimNode, userDataId: NimNode)
       of opChr:
         let ch = newLit(i.ch)
         quote do:
-          trace ms, `iname`, `opName`, s, "\"" & escapeChar(`ch`) & "\""
-          if si < s.len and s[si] == `ch`.char:
-            inc si
-            ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`, "\"" & escapeChar(`ch`) & "\""
+          if `si` < `s`.len and `s`[`si`] == `ch`.char:
+            inc `si`
+            `ip` = `ipNext`
           else:
-            ip = `ipFail`
+            `ip` = `ipFail`
 
       of opSet:
         let cs = newLit(i.cs)
         quote do:
-          trace ms, `iname`, `opName`, s, dumpSet(`cs`)
-          if si < s.len and s[si] in `cs`:
-            inc si
-            ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`, dumpSet(`cs`)
+          if `si` < `s`.len and `s`[`si`] in `cs`:
+            inc `si`
+            `ip` = `ipNext`
           else:
-            ip = `ipFail`
+            `ip` = `ipFail`
 
       of opSpan:
         let cs = newLit(i.cs)
         quote do:
-          trace ms, `iname`, `opName`, s, dumpSet(`cs`)
-          while si < s.len and s[si] in `cs`:
-            inc si
-          ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`, dumpSet(`cs`)
+          while `si` < `s`.len and `s`[`si`] in `cs`:
+            inc `si`
+          `ip` = `ipNext`
 
       of opChoice:
         let ip2 = newLit(ipNow + i.ipOffset)
         let siOffset = newLit(i.siOffset)
         quote do:
-          trace ms, `iname`, `opName`, s, $`ip2`
-          push(ms.backStack, BackFrame(ip:`ip2`, si:si+`siOffset`, rp:ms.retStack.top, cp:ms.capStack.top, pp:ms.precStack.top))
-          ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`, $`ip2`
+          push(`ms`.backStack, BackFrame(ip:`ip2`, si:`si`+`siOffset`, rp:`ms`.retStack.top, cp:`ms`.capStack.top, pp:`ms`.precStack.top))
+          `ip` = `ipNext`
 
       of opCommit:
         let ip2 = newLit(ipNow + i.ipOffset)
         quote do:
-          trace ms, `iname`, `opName`, s, $`ip2`
-          discard pop(ms.backStack)
-          ip = `ip2`
+          trace `ms`, `iname`, `opName`, `s`, $`ip2`
+          discard pop(`ms`.backStack)
+          `ip` = `ip2`
 
       of opCall:
         let label = newLit(i.callLabel)
         let ip2 = newLit(ipNow + i.callOffset)
         quote do:
-          trace ms, `iname`, `opName`, s, `label` & ":" & $`ip2`
-          push(ms.retStack, ip+1)
-          ip = `ip2`
+          trace `ms`, `iname`, `opName`, `s`, `label` & ":" & $`ip2`
+          push(`ms`.retStack, `ip`+1)
+          `ip` = `ip2`
 
       of opJump:
         let label = newLit(i.callLabel)
         let ip2 = newLit(ipNow + i.callOffset)
         quote do:
-          trace ms, `iname`, `opName`, s, `label` & ":" & $`ip2`
-          ip = `ip2`
+          trace `ms`, `iname`, `opName`, `s`, `label` & ":" & $`ip2`
+          `ip` = `ip2`
 
       of opCapOpen:
         let capKind = newLit(i.capKind)
         let capName = newLit(i.capName)
         let capSiOffset = newLit(i.capSiOffset)
         quote do:
-          trace ms, `iname`, `opName`, s, $`capKind` & " -> " & $si
-          push(ms.capStack, CapFrame(cft: cftOpen, si: si+`capSiOffset`, ck: `capKind`, name: `capName`))
-          ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`, $`capKind` & " -> " & $`si`
+          push(`ms`.capStack, CapFrame(cft: cftOpen, si: `si`+`capSiOffset`, ck: `capKind`, name: `capName`))
+          `ip` = `ipNext`
 
       of opCapClose:
         let ck = newLit(i.capKind)
@@ -212,121 +212,121 @@ proc genCasesCode*(program: Program, userDataType: NimNode, userDataId: NimNode)
           of ckAction:
             let code = rewriteCodeBlock(i.capAction)
             quote do:
-              trace ms, `iname`, `opName`, s, "ckAction -> " & $si
-              push(ms.capStack, CapFrame(cft: cftClose, si: si, ck: `ck`))
-              let capture {.inject.} = collectCaptures(fixCaptures(s, ms.capStack, FixOpen))
+              trace `ms`, `iname`, `opName`, `s`, "ckAction -> " & $`si`
+              push(`ms`.capStack, CapFrame(cft: cftClose, si: `si`, ck: `ck`))
+              let capture {.inject.} = collectCaptures(fixCaptures(`s`, `ms`.capStack, FixOpen))
               var ok = true
               template validate(o: bool) = ok = o
               template fail() = ok = false
-              template push(s: string) =
-                push(ms.capStack, CapFrame(cft: cftOpen, ck: ckStr))
-                push(ms.capStack, CapFrame(cft: cftClose, ck: ckStr, sPushed: s))
+              template push(`s`: string) =
+                push(`ms`.capStack, CapFrame(cft: cftOpen, ck: ckStr))
+                push(`ms`.capStack, CapFrame(cft: cftClose, ck: ckStr, sPushed: `s`))
               block:
                 `code`
               if ok:
-                ip = `ipNext`
+                `ip` = `ipNext`
               else:
-                ip = `ipFail`
+                `ip` = `ipFail`
 
           of ckRef:
             quote do:
-              trace ms, `iname`, `opName`, s, "ckRef -> " & $si
-              push(ms.capStack, CapFrame(cft: cftClose, si: si, ck: `ck`))
-              let r = collectCapturesRef(fixCaptures(s, ms.capStack, FixOpen))
-              ms.refs[r.key] = r.val
-              ip = `ipNext`
+              trace `ms`, `iname`, `opName`, `s`, "ckRef -> " & $`si`
+              push(`ms`.capStack, CapFrame(cft: cftClose, si: `si`, ck: `ck`))
+              let r = collectCapturesRef(fixCaptures(`s`, `ms`.capStack, FixOpen))
+              `ms`.refs[r.key] = r.val
+              `ip` = `ipNext`
 
           else:
             quote do:
-              trace ms, `iname`, `opName`, s, $`ck` & " -> " & $si
-              push(ms.capStack, CapFrame(cft: cftClose, si: si, ck: `ck`))
-              ip = `ipNext`
+              trace `ms`, `iname`, `opName`, `s`, $`ck` & " -> " & $`si`
+              push(`ms`.capStack, CapFrame(cft: cftClose, si: `si`, ck: `ck`))
+              `ip` = `ipNext`
 
       of opBackRef:
         let refName = newLit(i.refName)
         quote do:
-          if `refName` in ms.refs:
-            let s2 = ms.refs[`refName`]
-            trace ms, `iname`, `opName`, s, `refName` & ":\"" & s2 & "\""
-            if subStrCmp(s, s.len, si, s2):
-              inc si, s2.len
-              ip = `ipNext`
+          if `refName` in `ms`.refs:
+            let s2 = `ms`.refs[`refName`]
+            trace `ms`, `iname`, `opName`, `s`, `refName` & ":\"" & s2 & "\""
+            if subStrCmp(`s`, `s`.len, `si`, s2):
+              inc `si`, s2.len
+              `ip` = `ipNext`
             else:
-              ip = `ipFail`
+              `ip` = `ipFail`
           else:
             raise newException(NPegException, "Unknown back reference '" & `refName` & "'")
 
       of opErr:
         let msg = newLit(i.msg)
         quote do:
-          trace ms, `iname`, `opName`, s, `msg`
-          var e = newException(NPegException, "Parsing error at #" & $si & ": expected \"" & `msg` & "\"")
-          simax = max(simax, si)
-          e.matchLen = si
-          e.matchMax = simax
+          trace `ms`, `iname`, `opName`, `s`, `msg`
+          var e = newException(NPegException, "Parsing error at #" & $`si` & ": expected \"" & `msg` & "\"")
+          `simax` = max(`simax`, `si`)
+          e.matchLen = `si`
+          e.matchMax = `simax`
           raise e
 
       of opReturn:
         quote do:
-          if ms.retStack.top > 0:
-            trace ms, `iname`, `opName`, s
-            ip = pop(ms.retStack)
+          if `ms`.retStack.top > 0:
+            trace `ms`, `iname`, `opName`, `s`
+            `ip` = pop(`ms`.retStack)
           else:
-            trace ms, `iname`, `opName`, s
+            trace `ms`, `iname`, `opName`, `s`
             result.ok = true
-            simax = max(simax, si)
+            `simax` = max(`simax`, `si`)
             break
 
       of opAny:
         quote do:
-          trace ms, `iname`, `opName`, s
-          if si < s.len:
-            inc si
-            ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`
+          if `si` < `s`.len:
+            inc `si`
+            `ip` = `ipNext`
           else:
-            ip = `ipFail`
+            `ip` = `ipFail`
 
       of opNop:
         quote do:
-          trace ms, `iname`, `opName`, s
-          ip = `ipNext`
+          trace `ms`, `iname`, `opName`, `s`
+          `ip` = `ipNext`
 
       of opPrecPush:
         if i.prec == 0:
           quote do:
-            push(ms.precStack, 0)
-            ip = `ipNext`
+            push(`ms`.precStack, 0)
+            `ip` = `ipNext`
         else:
           let (iPrec, iAssoc) = (i.prec.newLit, i.assoc.newLit)
           if i.assoc == assocLeft:
             quote do:
-              if peek(ms.precStack) < `iPrec`:
-                push(ms.precStack, `iPrec`)
-                ip = `ipNext`
+              if peek(`ms`.precStack) < `iPrec`:
+                push(`ms`.precStack, `iPrec`)
+                `ip` = `ipNext`
               else:
-                ip = `ipFail`
+                `ip` = `ipFail`
           else:
             quote do:
-              if peek(ms.precStack) <= `iPrec`:
-                push(ms.precStack, `iPrec`)
-                ip = `ipNext`
+              if peek(`ms`.precStack) <= `iPrec`:
+                push(`ms`.precStack, `iPrec`)
+                `ip` = `ipNext`
               else:
-                ip = `ipFail`
+                `ip` = `ipFail`
 
       of opPrecPop:
         quote do:
-            discard ms.precStack.pop()
-            ip = `ipNext`
+            discard `ms`.precStack.pop()
+            `ip` = `ipNext`
 
       of opFail:
         quote do:
-          simax = max(simax, si)
-          if ms.backStack.top > 0:
-            trace ms, "", "opFail", s, "(backtrack)"
-            let t = pop(ms.backStack)
-            (ip, si, ms.retStack.top, ms.capStack.top, ms.precStack.top) = (t.ip, t.si, t.rp, t.cp, t.pp)
+          `simax` = max(`simax`, `si`)
+          if `ms`.backStack.top > 0:
+            trace `ms`, "", "opFail", `s`, "(backtrack)"
+            let t = pop(`ms`.backStack)
+            (`ip`, `si`, `ms`.retStack.top, `ms`.capStack.top, `ms`.precStack.top) = (t.ip, t.si, t.rp, t.cp, t.pp)
           else:
-            trace ms, "", "opFail", s, "(error)"
+            trace `ms`, "", "opFail", `s`, "(error)"
             break
 
     result.add nnkOfBranch.newTree(newLit(ipNow), call)
@@ -335,25 +335,25 @@ proc genCasesCode*(program: Program, userDataType: NimNode, userDataId: NimNode)
 # Generate code for tracing the parser. An empty stub is generated if tracing
 # is disabled
 
-proc genTraceCode*(program: Program, userDataType: NimNode, userDataId: NimNode): NimNode =
+proc genTraceCode*(program: Program, userDataType: NimNode, userDataId: NimNode, ms, s, si, simax, ip: NimNode): NimNode =
   
   when npegTrace:
     result = quote do:
-      proc doTrace(`ms`: var MatchState, iname, opname: string, s: Subject, msg: string) =
-          echo align(if ip >= 0: $ip else: "", 3) &
+      proc doTrace(`ms`: var MatchState, iname, opname: string, `s`: Subject, msg: string) =
+          echo align(if `ip` >= 0: $`ip` else: "", 3) &
             "|" & align($(peek(`ms`.precStack)), 3) &
-            "|" & align($si, 3) &
-            "|" & alignLeft(dumpString(s, si, 24), 24) &
+            "|" & align($`si`, 3) &
+            "|" & alignLeft(dumpString(`s`, `si`, 24), 24) &
             "|" & alignLeft(iname, 15) &
             "|" & alignLeft(opname & " " & msg, 40) &
             "|" & repeat("*", `ms`.backStack.top)
 
-      template trace(`ms`: var MatchState, iname, opname: string, s: Subject, msg = "") =
+      template trace(`ms`: var MatchState, iname, opname: string, `s`: Subject, msg = "") =
         doTrace(`ms`, iname, opname, `s`, msg)
 
   else:
     result = quote do:
-      template trace(ms: var MatchState, iname, opname: string, s: Subject, msg = "") =
+      template trace(`ms`: var MatchState, iname, opname: string, `s`: Subject, msg = "") =
         discard
 
 # Convert the list of parser instructions into a Nim finite state machine
@@ -362,10 +362,15 @@ proc genCode*(program: Program, userDataType: NimNode, userDataId: NimNode): Nim
 
   let
     count = program.patt.high
-    ms = ident "ms"
-    s = ident "s"
-    casesCode = genCasesCode(program, userdataType, userDataId)
-    traceCode = genTraceCode(program, userdataType, userDataId)
+    suffix = "_NP"
+    ms = ident "ms" & suffix
+    s = ident "s" & suffix
+    si = ident "si" & suffix
+    ip = ident "ip" & suffix
+    simax = ident "simax" & suffix
+
+    casesCode = genCasesCode(program, userdataType, userDataId, ms, s, si, simax, ip)
+    traceCode = genTraceCode(program, userdataType, userDataId, ms, s, si, simax, ip)
 
   # Generate the parser main loop. The .computedGoto.
   # pragma will generate code using C computed gotos, which will get highly
@@ -373,7 +378,7 @@ proc genCode*(program: Program, userDataType: NimNode, userDataId: NimNode): Nim
 
   when npegProfile:
     let loopCode = quote do:
-      profileLoop(`count`, casesCode, listing)
+      profileLoop(`count`, `casesCode`, listing)
   else:
     let loopCode = quote do:
       while true:
@@ -381,19 +386,19 @@ proc genCode*(program: Program, userDataType: NimNode, userDataId: NimNode): Nim
         `casesCode`
 
   # This is the result of genCode: a Parser object with a pointer to the
-  # generated proc below doing the matching
+  # generated proc below doing the matching.
 
   result = quote do:
 
-    let match = proc(`ms`: var MatchState, `s`: Subject, `userDataId`: var `userDataType`): MatchResult =
+    proc fn(`ms`: var MatchState, `s`: Subject, `userDataId`: var `userDataType`): MatchResult {.gensym.} =
 
       # Create local instances of performance-critical MatchState vars, this saves a
       # dereference on each access
 
       var
-        ip {.inject.}: range[0..`count`] = `ms`.ip
-        si {.inject.} = `ms`.si
-        simax {.inject.} = `ms`.simax
+        `ip`: range[0..`count`] = `ms`.ip
+        `si` = `ms`.si
+        `simax` = `ms`.simax
 
       `traceCode`
 
@@ -405,15 +410,15 @@ proc genCode*(program: Program, userDataType: NimNode, userDataId: NimNode): Nim
       # back, close the capture stack and collect all the captures in the match
       # result
 
-      `ms`.ip = ip
-      `ms`.si = si
-      `ms`.simax = simax
+      `ms`.ip = `ip`
+      `ms`.si = `si`
+      `ms`.simax = `simax`
       result.matchLen = `ms`.si
       result.matchMax = `ms`.simax
       if result.ok and `ms`.capStack.top > 0:
         result.cs = fixCaptures(`s`, `ms`.capStack, FixAll)
 
-    Parser[`userDataType`](fn: match)
+    Parser[`userDataType`](fn: fn)
 
   when npegExpand:
     echo result.repr
